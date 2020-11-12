18877fe6de584e480cc17be1f686459dde5fad4c
evolutionv5 <jorgeluiscastrov5@gmail.com>
2020-11-12 18:48:43 -0400

    refactorizacion de las respuestas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,47 +2,66 @@ import { Component, OnInit } from '@angular/core';
 import { Fraccion } from '../models/fraccion.interface';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  public fraccion: Fraccion;
-  public resultado1: Fraccion;
-  public resultado2: Fraccion;
-  public resultado3: Fraccion;
-  public resultado4: Fraccion;
-  public principal: Fraccion;
-  constructor() {}
-  ngOnInit() {
-    this.resultado1 = new Fraccion();
-    this.resultado2 = new Fraccion();
-    this.resultado3 = new Fraccion();
-    this.resultado4 = new Fraccion();
-    this.principal = new Fraccion();
-    this.principal = this.verificar(this.resultado1, this.resultado2);
-  }
-  mostrar() {
-    this.fraccion = new Fraccion();
-    this.fraccion.mostrar();
-  }
-  verificar(x: Fraccion, y: Fraccion): Fraccion {
-    let resD = x.denominador * y.denominador;
-    let resN =
-      (resD / x.denominador) * x.numerador +
-      (resD / y.denominador) * y.numerador;
-    x.mostrar();
-    y.mostrar();
-    console.log('denominador: ', resD, 'numerador: ', resN);
-    for (let i = 2; i < 7; i++) {
-      if (resD % i === 0 && resN % i === 0) {
-        resD /= i;
-        resN /= i;
-      }
-    }
-    const auxFraccion: Fraccion = new Fraccion();
-    auxFraccion.numerador = resN;
-    auxFraccion.denominador = resD;
-    return auxFraccion;
-  }
+	public fraccion: Fraccion;
+	public resultado: Fraccion;
+	public resultado1: Fraccion;
+	public resultado2: Fraccion;
+	public resultado3: Fraccion;
+	public resultado4: Fraccion;
+	public principal: Fraccion;
+	public arrayFraccion: Fraccion[];
+	constructor() {}
+	ngOnInit() {
+		this.generarFracciones();
+	}
+	mostrar() {
+		this.fraccion = new Fraccion();
+		this.fraccion.mostrar();
+	}
+	verificar(x: Fraccion, y: Fraccion): Fraccion {
+		let resD = x.denominador * y.denominador;
+		let resN =
+			(resD / x.denominador) * x.numerador +
+			(resD / y.denominador) * y.numerador;
+		x.mostrar();
+		y.mostrar();
+		for (let i = 2; i < 7; i++) {
+			if (resD % i === 0 && resN % i === 0) {
+				resD /= i;
+				resN /= i;
+			}
+		}
+		const auxFraccion: Fraccion = new Fraccion();
+		auxFraccion.numerador = resN;
+		auxFraccion.denominador = resD;
+		return auxFraccion;
+	}
+	generarFracciones() {
+		this.arrayFraccion = [];
+		for (let i = 0; this.arrayFraccion.length < 4; i++) {
+			this.resultado = new Fraccion();
+			if (!this.verificaIgualdad(this.arrayFraccion, this.resultado)) {
+				if (this.resultado.denominador !== this.resultado.numerador) {
+					this.arrayFraccion.push(this.resultado);
+				}
+			}
+		}
+	}
+
+	verificaIgualdad(arr: Fraccion[], fr2: Fraccion) {
+		for (const fr1 of arr) {
+			if (
+				fr1.numerador === fr2.numerador &&
+				fr1.denominador === fr2.denominador
+			) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
--- src/app/models/fraccion.interface.ts
+++ src/app/models/fraccion.interface.ts
@@ -1,12 +1,42 @@
 export class Fraccion {
-  numerador: number;
-  denominador: number;
-  constructor() {
-    this.numerador = Math.round(Math.random() * 9) + 1;
-    this.denominador = Math.round(Math.random() * 9) + 1;
-  }
-  mostrar() {
-    console.log('num: ', this.numerador);
-    console.log('dem: ', this.denominador);
-  }
+	numerador: number;
+	denominador: number;
+	constructor() {
+		this.numerador = Math.round(Math.random() * 9) + 1;
+		this.denominador = Math.round(Math.random() * 9) + 1;
+		this.simplificar();
+	}
+	mostrar() {
+		console.log('num: ', this.numerador);
+		console.log('dem: ', this.denominador);
+	}
+	simplificar() {
+		let sw = true;
+		while (sw) {
+			if (this.denominador % 2 === 0 && this.numerador % 2 === 0) {
+				this.denominador /= 2;
+				this.numerador /= 2;
+			} else {
+				sw = false;
+			}
+		}
+		sw = true;
+		while (sw) {
+			if (this.denominador % 3 === 0 && this.numerador % 3 === 0) {
+				this.denominador /= 3;
+				this.numerador /= 3;
+			} else {
+				sw = false;
+			}
+		}
+		sw = true;
+		while (sw) {
+			if (this.denominador % 5 === 0 && this.numerador % 5 === 0) {
+				this.denominador /= 5;
+				this.numerador /= 5;
+			} else {
+				sw = false;
+			}
+		}
+	}
 }
