6afa34fb0b6dee7399a249247b768206ef59cc4c
jorgeCastro <jorgeluiscastrov5@gmail.com>
2021-08-04 17:38:28 -0400

    fracciones hechas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,61 +7,55 @@ import { Fraccion } from '../models/fraccion.interface';
 	styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-	public fraccion: Fraccion;
+	public pregunta1: Fraccion = new Fraccion();
+	public pregunta2: Fraccion = new Fraccion();
+	public arrayOpciones: Fraccion[] = [];
 	public resultado: Fraccion;
-	public resultado1: Fraccion;
-	public resultado2: Fraccion;
-	public resultado3: Fraccion;
-	public resultado4: Fraccion;
-	public principal: Fraccion;
-	public arrayFraccion: Fraccion[];
-	constructor() {}
+	public swRespuesta = false;
+
+	constructor() {
+		this.pregunta1.crearPregunta();
+		this.pregunta2.crearPregunta();
+	}
+
 	ngOnInit() {
-		this.generarFracciones();
+		this.generarOpciones();
 	}
-	mostrar() {
-		this.fraccion = new Fraccion();
-		this.fraccion.mostrar();
-	}
-	verificar(x: Fraccion, y: Fraccion): Fraccion {
-		let resD = x.denominador * y.denominador;
-		let resN =
-			(resD / x.denominador) * x.numerador +
-			(resD / y.denominador) * y.numerador;
-		x.mostrar();
-		y.mostrar();
-		for (let i = 2; i < 7; i++) {
-			if (resD % i === 0 && resN % i === 0) {
-				resD /= i;
-				resN /= i;
-			}
-		}
-		const auxFraccion: Fraccion = new Fraccion();
-		auxFraccion.numerador = resN;
-		auxFraccion.denominador = resD;
-		return auxFraccion;
-	}
-	generarFracciones() {
-		this.arrayFraccion = [];
-		for (let i = 0; this.arrayFraccion.length < 4; i++) {
-			this.resultado = new Fraccion();
-			if (!this.verificaIgualdad(this.arrayFraccion, this.resultado)) {
-				if (this.resultado.denominador !== this.resultado.numerador) {
-					this.arrayFraccion.push(this.resultado);
-				}
+
+	generarOpciones() {
+		this.resultado = this.sumarFracciones(this.pregunta1, this.pregunta2);
+		this.arrayOpciones.push(this.resultado);
+		while (this.arrayOpciones.length < 4) {
+			const auxFraccion = new Fraccion();
+			if (!this.sonIguales(auxFraccion, this.resultado) && auxFraccion.denominador !== auxFraccion.numerador) {
+				console.log(auxFraccion);
+				this.arrayOpciones.push(auxFraccion);
 			}
 		}
 	}
 
-	verificaIgualdad(arr: Fraccion[], fr2: Fraccion) {
-		for (const fr1 of arr) {
-			if (
-				fr1.numerador === fr2.numerador &&
-				fr1.denominador === fr2.denominador
-			) {
-				return true;
-			}
+	sonIguales(fr1: Fraccion, fr2: Fraccion) {
+		if (fr1.numerador === fr2.numerador && fr1.denominador === fr2.denominador) {
+			return true;
 		}
 		return false;
+	}
+
+	sumarFracciones(fr1: Fraccion, fr2: Fraccion): Fraccion {
+		const DENOMINADOR = fr1.denominador * fr2.denominador;
+		const NUMERADOR = (DENOMINADOR / fr1.denominador) * fr1.numerador + (DENOMINADOR / fr2.denominador) * fr2.numerador;
+		const fraccion = new Fraccion();
+		fraccion.denominador = DENOMINADOR;
+		fraccion.numerador = NUMERADOR;
+		fraccion.simplificar();
+		return fraccion;
+	}
+
+	verificaRespuesta(fr: Fraccion) {
+		if (fr.denominador === this.resultado.denominador && fr.numerador === this.resultado.numerador) {
+			this.swRespuesta = true;
+		} else {
+			this.swRespuesta = false;
+		}
 	}
 }
--- src/app/models/fraccion.interface.ts
+++ src/app/models/fraccion.interface.ts
@@ -1,41 +1,54 @@
 export class Fraccion {
-	numerador: number;
-	denominador: number;
+	public numerador: number;
+	public denominador: number;
+
 	constructor() {
+		this.numerador = Math.round(Math.random() * 49) + 1;
+		this.denominador = Math.round(Math.random() * 49) + 1;
+		this.simplificar();
+	}
+
+	crearPregunta() {
 		this.numerador = Math.round(Math.random() * 9) + 1;
 		this.denominador = Math.round(Math.random() * 9) + 1;
 		this.simplificar();
 	}
+
 	mostrar() {
 		console.log('num: ', this.numerador);
 		console.log('dem: ', this.denominador);
 	}
+
 	simplificar() {
-		let sw = true;
-		while (sw) {
-			if (this.denominador % 2 === 0 && this.numerador % 2 === 0) {
-				this.denominador /= 2;
+		let hayMultiplos = true;
+		while (hayMultiplos && this.denominador !== 1 && this.numerador !== 1) {
+			if (this.numerador % this.denominador === 0) {
+				this.numerador /= this.denominador;
+				this.denominador /= this.denominador;
+			} else if (this.denominador % this.numerador === 0) {
+				this.numerador /= this.numerador;
+				this.denominador /= this.numerador;
+			} else {
+				hayMultiplos = false;
+			}
+			this.simplificarDoble();
+		}
+	}
+
+	simplificarDoble() {
+		let hayMultiplos = true;
+		while (hayMultiplos) {
+			if (this.numerador % 2 === 0 && this.denominador % 2 === 0) {
 				this.numerador /= 2;
-			} else {
-				sw = false;
-			}
-		}
-		sw = true;
-		while (sw) {
-			if (this.denominador % 3 === 0 && this.numerador % 3 === 0) {
-				this.denominador /= 3;
+				this.denominador /= 2;
+			} else if (this.numerador % 3 === 0 && this.denominador % 3 === 0) {
 				this.numerador /= 3;
-			} else {
-				sw = false;
-			}
-		}
-		sw = true;
-		while (sw) {
-			if (this.denominador % 5 === 0 && this.numerador % 5 === 0) {
-				this.denominador /= 5;
+				this.denominador /= 3;
+			} else if (this.numerador % 5 === 0 && this.denominador % 5 === 0) {
 				this.numerador /= 5;
+				this.denominador /= 5;
 			} else {
-				sw = false;
+				hayMultiplos = false;
 			}
 		}
 	}
